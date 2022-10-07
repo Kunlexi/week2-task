50cc9062c9cd56866b7d75336d4e2aa4c16902ff
kunlexi <adesinataofik5.com>
2022-10-07 16:06:36 +0100

    commitf

--- week-2-node-sq012-Kunlexi-main/src/classifier.js
+++ week-2-node-sq012-Kunlexi-main/src/classifier.js
@@ -1,3 +1,74 @@
-function classifier(input) {}
+function classifier(input) {
+  // Duplicating the array to avoid immutation
+  // Because i don't want to tamper the original object
+  let studentArray = [...input];
+  // Throw error if input is not equal to object
+  if (typeof input !== "object") {
+    throw new Error("Invalid Input");
+  }
+  // Map through the student array to get the student ages in an array
+  studentArray = studentArray.map(eachStudent => {
+    eachStudent.age = new Date().getFullYear() - new Date(eachStudent.dob).getFullYear();
+    return eachStudent;
+  });
+  // Sort student array by age
+  studentArray = studentArray.sort((a, b) => a.age - b.age);
+  // Set student group array to empty
+  let studentGroup = [];
+  // Set student age group array to empty
+  let studentAgeGroup = [];
+  // Set age limit initially to 0
+  let limit = 0;
+  // Loop through the student array again
+  for (let i = 0; i < studentArray.length; i++) {
+    // Set each element to person
+    let person = studentArray[i];
+    // Check the limit for the first time if its zero
+    if (limit === 0) {
+      // If true set limit to person age
+      limit = person.age;
+    }
+    // Now access the person age and limits, to check the ones that are less than five
+    // And also set condition to make sure that the student are not more than 3 in group
+    if (person.age - limit <= 5 && studentAgeGroup.length < 3) {
+      // Push those that return true into the student age group
+      studentAgeGroup.push(person)
+    } else {
+      // Push those that return false for the first statement into student group
+      studentGroup.push(studentAgeGroup);
+      studentAgeGroup = [];
+      studentAgeGroup.push(person)
+      limit = person.age;
+    }
+  }
+  // Push the student age group into the student group.
+  if (studentAgeGroup.length) studentGroup.push(studentAgeGroup);
+  // Set the result to empty array
+  let result = {};
+  // Loop through the student group to arrange the already create group in to object
+  for (let i = 0; i < studentGroup.length; i++) {
+    // Arrange the groups into different groups (group 1, gtoup 2, .....)
+    let res = "group" + (i + 1)
+    // Get the age of students in the groups by group
+    let eachAge = studentGroup[i].map(e => e.age)
+    eachAge;
+    // Setting the values in each goups
+    result[res] = {
+      // Members object
+      members: studentGroup[i],
+      // Age of the oldest student in the group
+      oldest: Math.max(...eachAge),
+      // Sum the age of the students in the group
+      sum: eachAge.reduce((num, tot) => num + tot, 0),
+      // Students reg numbers
+      regNos: studentGroup[i].map(e => parseInt(e.regNo)).sort((a, b) => a - b)
+    }
+  }
+  // Return the result
+  result.noOfGroups = studentGroup.length;
+  return result;
 
-export default classifier;
+
+}
+
+module.exports = classifier;
--- week-2-node-sq012-Kunlexi-main/src/duplicate-transactions.js
+++ week-2-node-sq012-Kunlexi-main/src/duplicate-transactions.js
@@ -1,3 +1,110 @@
-function findDuplicateTransactions(transactions) {}
+// Object properties to be match
+const propertiesToMatch = ['sourceAccount', 'targetAccount', 'amount', 'category'];
+
+// Helper Functions
+// Comparing the elements of object
+const compareTransaction = (transaction1, transaction2, transactionObject) => transactionObject.every(object => transaction1[object] === transaction2[object]);
+
+// Checking for transaction less than 1 minute
+const isOneMinute = (transaction1, transaction2) => Math.abs(new Date(transaction1.time) - new Date(transaction2.time)) < 60000;
+
+// Checking for Duplicate transaction that match the transaction properties and is within 0 - 59 seconds
+const isDuplicate = (transaction1, transactions2) => compareTransaction(transaction1, transactions2, propertiesToMatch) && isOneMinute(transaction1, transactions2);
+
+// Helper function for grouping the duplicates
+function groupDuplicateTransactions(duplicates) {
+  // Set sub array into empty brackets
+  const subArrays = [];
+
+  // Loop through the duplicates
+  for(let index = 1, previousDuplicate = false, duplicateIndex = -1; index < duplicates.length; index++) {
+    // Use a nested if statements
+    // If the first transaction in the array and any other transaction in the array is an exact duplicate
+    if(isDuplicate(duplicates[index], duplicates[index - 1])) {
+      //
+      if(!previousDuplicate) {
+        duplicateIndex++;
+      }
+      // If there is no duplicate in the sub array then set it to empty
+      if(!subArrays[duplicateIndex]) subArrays[duplicateIndex] = [];
+
+      // Then push the last element of the duplicate sub array of duplicates.
+      subArrays[duplicateIndex].push(duplicates[index - 1]);
+
+      // If transaction is equal to the last duplicates then push that transaction into the subarray
+      if(index === duplicates.length - 1) subArrays[duplicateIndex].push(duplicates[index]);
+
+      // Then return back to the state to continue the loop until its over.
+      previousDuplicate = true;
+    } else {
+      // If there is previous duplicates the just push it to the subarray of duplicate
+      if(previousDuplicate) subArrays[duplicateIndex].push(duplicates[index - 1]);
+
+      // And return previous duplicate as false
+      previousDuplicate = false;
+    }
+  }
+  // Then return the sub arrays
+  return subArrays;
+
+}
+
+// Duplicate transaction function (MAIN FUNCTION)
+// Filtering the valid transactions and sorting transaction into group of objects
+function findDuplicateTransactions(transactions) {
+
+  // Check if transactions is not an object
+  if(typeof transactions !== "object"){
+    throw new Error ("");
+  }
+  // Spread the transations object to avoid mutation of object
+  const transactionsObject = [...transactions]
+
+     // Sort the transaction by the properties
+    .sort((transaction1, transaction2) => {
+
+      // Checking if the first transaction for source account picked is not equal to any other transaction
+      if(transaction1.sourceAccount !== transaction2.sourceAccount) {
+
+        // Then return 1 for each of the transaction found greater another transaction
+        let sourceAcoountGroup = transaction1.sourceAccount > transaction2.sourceAccount ? 1 : -1;
+        return sourceAcoountGroup;
+      }
+
+      // Checking if the first transaction for target account picked is not equal to any other transaction
+      else if(transaction1.targetAccount !== transaction2.targetAccount) {
+
+        // Then return 1 for each of the transaction found greater than another transaction
+        let targetAccountGroup = transaction1.targetAccount > transaction2.targetAccount ? 1 : -1;
+        return targetAccountGroup;
+      }
+
+      // Checking if the first transaction for amount picked is not equal to any other transaction
+      else if(transaction1.amount !== transaction2.amount) {
+
+        // Then return 1 for each of the transaction found greater another transaction
+        let amountGroup = transaction1.amount > transaction2.amount ? 1 : -1;
+        return amountGroup;
+      }
+
+      // Checking if the first transaction for category picked is not equal to any other transaction
+      else if (transaction1.category !== transaction2.category) {
+        let categoryGroup = transaction1.category > transaction2.category ? 1 : -1;
+        return categoryGroup;
+      }
+      
+      // Checking if the first transaction for source account picked is not equal to any other transaction
+      else if(transaction1.time !== transaction2.time) {
+        let timeGroup = transaction1.time > transaction2.time ? 1 : -1;
+        return timeGroup;
+        
+      }
+
+  });
+  // Return the grouped duplicate after sorting with there id
+  return groupDuplicateTransactions(transactionsObject).sort((transaction1, transaction2) => transaction1[0].id - transaction2[0].id);
+
+}
+
 
 export default findDuplicateTransactions;
